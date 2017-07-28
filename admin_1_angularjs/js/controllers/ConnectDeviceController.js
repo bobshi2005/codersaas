51d02bc2259d45ed4866e16bbd4d600e89434cf5
jianpingli <870158817@qq.com>
2017-07-28 15:37:39 +0800

    添加传感器列表

--- admin_1_angularjs/js/controllers/ConnectDeviceController.js
+++ admin_1_angularjs/js/controllers/ConnectDeviceController.js
@@ -1,17 +1,53 @@
-angular.module('MetronicApp').controller('ConnectDeviceController', ['$scope', '$rootScope', 'deviceApi','$stateParams',function($scope, $rootScope, deviceApi, $stateParams) {
+angular.module('MetronicApp').controller('ConnectDeviceController', ['$scope', '$rootScope', 'deviceApi','$stateParams','NgTableParams','$element',function($scope, $rootScope, deviceApi, $stateParams,NgTableParams,$element) {
     $rootScope.menueName = 'sidebar-asset';
     $scope.menueName = $rootScope.menueName;
     $scope.equipmentId = $stateParams.equipmentId;
     $scope.equipmentname = $stateParams.name;
     $scope.protocolId = $stateParams.protocolId;
     $scope.heartData = $stateParams.heartData;
-    console.log('protocolId',$stateParams.protocolId);
+    $scope.propertylist=[];
+    $scope.equipmentModelId = $stateParams.equipmentModelId;
+    console.log('equipmentModelId',$stateParams.equipmentModelId);
 
     $scope.protocolLists =[
       {"id":1,"name":"MB RTU"},
       {"id":2,"name":"MB TCP"},
       {"id":3,"name":"MQTT"}
     ];
+
+    $scope.checkboxes = {
+      checked: false,
+      items: {}
+    };
+
+    //监听 checkbox
+    $scope.$watch(function() {
+      return $scope.checkboxes.checked;
+    }, function(value) {
+      angular.forEach($scope.propertylist, function(item) {
+        $scope.checkboxes.items[item.equipmentModelPropertyId] = value;
+      });
+    });
+
+    // watch for data checkboxes
+   $scope.$watch(function() {
+     return $scope.checkboxes.items;
+   }, function(values) {
+     var checked = 0, unchecked = 0,
+     total = $scope.propertylist.length;
+     angular.forEach($scope.checkboxes.items, function(item) {
+       if(item){
+         checked += 1;
+       }else{
+         unchecked +=1;
+       }
+     });
+     if ((unchecked == 0) || (checked == 0)) {
+       $scope.checkboxes.checked = (checked == total && total>0);
+     }
+    //  grayed checkbox
+     angular.element($element[0].getElementsByClassName("select-all")).prop("indeterminate", (checked != 0 && unchecked != 0));
+   }, true);
 
     $scope.accessdev ={ip:'127.0.0.1', port:'8234'};
 
@@ -26,6 +62,7 @@ angular.module('MetronicApp').controller('ConnectDeviceController', ['$scope', '
         accessDevice();
       }
     };
+    getmodelPropertylist();
 
     function accessDevice() {
         var params={};
@@ -43,4 +80,30 @@ angular.module('MetronicApp').controller('ConnectDeviceController', ['$scope', '
                 alert('网络连接问题，请稍后再试！');
             });
     };
+
+    function getmodelPropertylist(){
+      $scope.propertylist=[];
+      $scope.checkboxes.checked = false;
+      $scope.checkboxes.items = {};
+      $scope.tableParams = new NgTableParams({
+        page: 1,
+        count:2
+      }, {
+        counts:[2,5,10],
+        getData: function(params) {
+          return deviceApi.getmodelPropertylist($scope.equipmentModelId,'asc', (params.page()-1)*params.count(), params.count())
+            .then(function(result) {
+                if(result.data.total > 0) {
+                     $scope.propertylist=result.data.rows;
+                }else {
+                  $scope.propertylist=[];
+                }
+                params.total(result.data.total);
+                return $scope.propertylist;
+            }, function(err) {
+            });
+        }
+      });
+      $scope.tableParams.reload();
+    }
 }]);
--- admin_1_angularjs/js/main.js
+++ admin_1_angularjs/js/main.js
@@ -484,7 +484,7 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
         })
         .state('main.asset.connectdevice', {
             url: "/connectdevice",
-            params:{"equipmentId":null, "name": null, "heartData":null, "protocolId":null},
+            params:{"equipmentId":null, "name": null, "heartData":null, "protocolId":null, "equipmentModelId": null},
             templateUrl: "views/asset-connectdevice.html?version=2017071304",
             controller: 'ConnectDeviceController',
             resolve: {
