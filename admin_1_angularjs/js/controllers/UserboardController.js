ab6044e0696a88c80529333a08d00489e1bc8aee
jianpingli <870158817@qq.com>
2017-09-19 21:13:15 +0800

    用户信息显示完善

--- admin_1_angularjs/js/controllers/UserboardController.js
+++ admin_1_angularjs/js/controllers/UserboardController.js
@@ -1,6 +1,27 @@
-angular.module('MetronicApp').controller('UserboardController', ['$scope', '$rootScope', function($scope, $rootScope) {
+angular.module('MetronicApp').controller('UserboardController', ['$scope', '$rootScope','userApi','locals',function($scope, $rootScope,userApi,locals) {
   $rootScope.menueName = 'sidebar-setting';
   $scope.menueName = $rootScope.menueName;
   // $rootScope.settings.layout.pageSidebarClosed = false;
   // Cookies.set('sidebar_closed', '1');
-  }]);
+  $scope.info =[];
+  $scope.$on('$viewContentLoaded', function() {
+    var phone = locals.get("username");
+    getuserInfo(phone);
+  });
+
+  function getuserInfo(phone){
+    userApi.userInfo(phone)
+    .then(function(result) {
+        if(result.data.code == 1) {
+          console.log('loginInfo',result.data);
+          $scope.info = result.data.data;
+        }else {
+          $scope.info =[];
+          console.log(result.data.data);
+        }
+    }, function(err) {
+        console.log('getUserInfoerr',err);
+    });
+  }
+
+}]);
